99c137e852090d3660105cf52778b80ad17986cb
kiwiapple87 <mrorange4287@gmail.com>
2016-09-14 05:53:51 -0500

    Initial commit

---
+++ CloudripMountain/to arms.py
@@ -0,0 +1,27 @@
+# Ogres are going to attack soon.
+# Move near each of tents (to the X marks)
+# say() something at each X to wake your soldiers.
+# Beware: leave the camp when the battle begins!
+# Ogres will send reinforcements if they see the hero.
+
+# The sergeant knows the distance between tents.
+sergeant =  hero.findNearest(hero.findFriends())
+
+# The distances between the X marks.
+stepX = sergeant.tentDistanceX
+stepY = sergeant.tentDistanceY
+# The number of tents.
+tentsInRow = 5
+tentsInColumn = 4
+
+# The first tent mark has constant coordinates.
+firstX = 10
+firstY = 14
+
+# Use nested loops and visit all 20 tents.
+# IMPORTANT: move row by row - it's faster.
+
+        # Move at the marks near tents and say anything.
+
+
+# Now watch the battle.
