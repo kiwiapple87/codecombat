6f84066665da49683337efa7d6d6f48f8cd8667d
kiwiapple87 <mrorange4287@gmail.com>
2016-09-22 05:58:08 -0500

    Initial commit

---
+++ CloudripMountain/The Hunt Begins.py
@@ -0,0 +1,24 @@
+# Senick is trying to find the elusive Burleous Majoris!
+# But he doesn't know how big a Burleous Majoris would be...
+# Find the average size of this burl population to use as a baseline!
+
+# This function returns average size of all the burls in an array.
+def averageSize(burls):
+    sum = sumSize(burls)
+    # Remember the average is the sum of the parts divided by the amount!
+    return sum / burls.length
+
+# This function should return the sum of all the burls sizes.
+def sumSize(burls):
+    # Implement the sum function using the burls 'size':
+    sum = 0
+    for i in burls:
+        sum = sum + i.health
+    return sum
+
+while True:
+    burls = findByType("burls", findFriends())
+    # Find the average size of the burls by calling the 'averageSize' function.
+    size = averageSize(burls)
+    # Say the average size of the seen burls!
+    hero.say(size)
